108a9fe551076fb4f21ef8f5be4e491f79c9b988
Johnny Herrera <jmjaramillo95@gmail.com>
2025-10-01 23:23:53 -0500

    propertyChanged

---
+++ dependencyProperties/JuntaNombre.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dependencyProperties
+{
+    public class JuntaNombre :INotifyPropertyChanged
+    {
+        private string nombre, apellido, nombre_completo;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; } set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Nombre_completo
+        {
+            get { return nombre_completo = Nombre + " " + Apellido; }
+            set {  }
+        }
+    }
+}
